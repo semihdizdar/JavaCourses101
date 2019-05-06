67ef0f1be2880a5a6850d09ee7d53c757333acf8
Semih DİZDAR <semih.dizdar@bil.omu.edu.tr>
2019-05-07 00:32:15 +0300

    ilk push işlemi

---
+++ firstAppGithub/src/test/java/com/semihdizdar/App.java
@@ -0,0 +1,25 @@
+package com.semihdizdar;
+
+
+
+public class App 
+{
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Merhaba");
+		
+		islemler islem = new islemler();
+		islem.goster();
+		selamla();
+	}
+
+	
+	
+	private static void selamla() {
+		// TODO Auto-generated method stub
+		System.out.println("Selam Github");
+		
+	}
+
+}
---
+++ firstAppGithub/src/test/java/com/semihdizdar/islemler.java
@@ -0,0 +1,11 @@
+package com.semihdizdar;
+
+public class islemler {
+	
+	int veriler [] = {1,2,3,4} ;
+	
+	public void goster() {
+		System.out.println("Sonucları göster");
+	}
+
+}
